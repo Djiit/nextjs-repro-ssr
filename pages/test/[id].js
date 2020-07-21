47512d98b09e47e1368c895ff439015e0477b2ef
Julien Tanay <julien.tanay@gmail.com>
2020-07-21 16:59:37 +0200

    Add getServerSideProps example

---
+++ pages/test/[id].js
@@ -0,0 +1,49 @@
+export default function Test(props) {
+  return (
+    <div className="container">
+      <main>Hello {props.id}</main>
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  return {
+    props: {
+      id: context.params.id,
+    }, // will be passed to the page component as props
+  };
+}
